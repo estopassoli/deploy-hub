880af929b29107f2fec35c67afc9489bfd388890
estopassoli <itsdenahi@gmail.com>
2025-12-08 18:09:45 -0300

    logs bugfix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
 import { AppsModule } from './apps/apps.module';
+import { AuthModule } from './auth/auth.module';
 import { DeployModule } from './deploy/deploy.module';
-import { WebhookModule } from './webhook/webhook.module';
-import { SystemModule } from './system/system.module';
-import { MetricsModule } from './metrics/metrics.module';
 import { EmailModule } from './email/email.module';
+import { LogsModule } from './logs/logs.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { SystemModule } from './system/system.module';
 import { TerminalModule } from './terminal/terminal.module';
+import { WebhookModule } from './webhook/webhook.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { TerminalModule } from './terminal/terminal.module';
     MetricsModule,
     EmailModule,
     TerminalModule,
+    LogsModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
